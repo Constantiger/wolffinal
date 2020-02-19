39f590f92c7b7c3101382b6018d468dd85dad395
Stomach Diego <sdiego@oa-k5.21-school.ru>
2020-02-19 12:35:19 +0300

    norm main.c

--- main.c
+++ main.c
@@ -6,106 +6,106 @@
 /*   By: aannara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 18:07:03 by aannara           #+#    #+#             */
-/*   Updated: 2020/02/18 18:07:58 by aannara          ###   ########.fr       */
+/*   Updated: 2020/02/19 12:34:00 by sdiego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
-
 #include "stdio.h"
 
-void    set_tang(t_sdl *s)
+void	set_tang(t_sdl *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    s->tngm = PI2 / s->ang;
-    s->pi = s->tngm / 2;
-    s->hpi = s->pi / 2;
-    s->pihpi = s->tngm * 3 / 4;
+	i = 0;
+	s->tngm = PI2 / s->ang;
+	s->pi = s->tngm / 2;
+	s->hpi = s->pi / 2;
+	s->pihpi = s->tngm * 3 / 4;
 	s->qpi = s->tngm / 8;
 	s->qpi3 = s->qpi * 3;
 	s->qpi5 = s->qpi * 5;
 	s->qpi7 = s->qpi * 7;
-    s->tang = (float*)malloc((s->tngm) * sizeof(float));
-    if (s->tang == NULL)
-    {
-        write(1, "tang array error.\n", 18);
-        quit(s);
-        exit(1);
-    }
-    while (i < s->tngm)
-    {
-        s->tang[i] = tan(i * s->ang);
-        i++;
-    }
+	s->tang = (float*)malloc((s->tngm) * sizeof(float));
+	if (s->tang == NULL)
+	{
+		write(1, "tang array error.\n", 18);
+		quit(s);
+		exit(1);
+	}
+	while (i < s->tngm)
+	{
+		s->tang[i] = tan(i * s->ang);
+		i++;
+	}
 }
 
-void    print_map(t_sdl *s)
+void	print_map(t_sdl *s)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    j = 0;
-    while (j < s->msy)
-    {
-        while (i < s->msx)
-        {
-            write(1, &s->m[i][j], 1);
-            i++;
-        }
-        write(1, "\n", 1);
-        j++;
-        i = 0;
-    }
+	i = 0;
+	j = 0;
+	while (j < s->msy)
+	{
+		while (i < s->msx)
+		{
+			write(1, &s->m[i][j], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
+		i = 0;
+	}
 }
 
-char      check(t_vect *r)
+char	check(t_vect *r)
 {
-    char    flag;
+	char	flag;
 
-    flag = 0;
-    if (r->v[0] > 100)
-    {
-        flag = 1;
-        r->v[0] = 100;
-    }
-    if (r->v[0] < 0)
-    {
-        flag = 1;
-        r->v[0] = 0;
-    }
-    if (r->v[1] > 100)
-    {
-        flag = 1;
-        r->v[1] = 100;
-    }
-    if (r->v[1] < 0)
-    {
-        flag = 1;
-        r->v[1] = 0;
-    }
-    return (flag);
+	flag = 0;
+	if (r->v[0] > 100)
+	{
+		flag = 1;
+		r->v[0] = 100;
+	}
+	if (r->v[0] < 0)
+	{
+		flag = 1;
+		r->v[0] = 0;
+	}
+	if (r->v[1] > 100)
+	{
+		flag = 1;
+		r->v[1] = 100;
+	}
+	if (r->v[1] < 0)
+	{
+		flag = 1;
+		r->v[1] = 0;
+	}
+	return (flag);
 }
 
-void    swapi(int *a, int *b)
+void	swapi(int *a, int *b)
 {
-    int c;
+	int	c;
 
-    c = *a;
-    *a = *b;
-    *b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    t_sdl   sdl;
-    if(init(&sdl))
-    {
+	t_sdl	sdl;
+
+	if (init(&sdl))
+	{
 		write(1, "init error\n", 11);
-        quit(&sdl);
-    }
+		quit(&sdl);
+	}
 	set(&sdl);
 	if (!load_map(&sdl))
 		quit(&sdl);
@@ -114,17 +114,17 @@ int main(int argc, char **argv)
 	set_tang(&sdl);
 	load_audio(&sdl);
 	load_textures(&sdl);
-    print_map(&sdl);
+	print_map(&sdl);
 	SDL_ShowCursor(SDL_DISABLE);
-	if( Mix_PlayingMusic() == 0 )
+	if (Mix_PlayingMusic() == 0)
 	{
-		if (Mix_PlayMusic( sdl.gmusic, -1) == -1)
-            {
-                write(1, "error play mus\n", 15);
-                quit(&sdl);
-            }
+		if (Mix_PlayMusic(sdl.gmusic, -1) == -1)
+		{
+			write(1, "error play mus\n", 15);
+			quit(&sdl);
+		}
 		Mix_VolumeMusic(MIX_MAX_VOLUME / 12);
 	}
 	loop(&sdl);
-    return (0);
+	return (0);
 }
